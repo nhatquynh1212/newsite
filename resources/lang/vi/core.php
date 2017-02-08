6af477647a0e348a392b2692d2825be059edf509
nhatquynh1212 <nhatquynh1212@gmail.com>
2017-02-08 08:07:43 +0700

    first commit

---
+++ resources/lang/vi/core.php
@@ -0,0 +1,190 @@
+<?php
+
+return array(
+// General
+	"norecord" => "Không lưu",
+	"create" => "Tạo mới",
+
+	// General , Login Info & Signup
+	"home" => "Trang chủ",
+	"group" => "Nhóm",
+	"username" => "ID",
+	"email" => "Địa chỉ email",
+	"password" => "Mật khẩu",
+	"repassword" => "Xác nhận mật khẩu",
+	"forgotpassword" => "Quên mật khẩu",
+	"newpassword" => "Mật khẩu mới",
+	"conewpassword" => "Xác nhận mật khẩu",
+	"notepassword" => "Để trống nếu bạn không muốn thay đổi mật khẩu", // updated apidevlab
+	"submit" => "Gửi",
+	"signin" => "Đăng Nhập",
+	"signup" => "Đăng Ký",
+	"subscribe" => "Theo Dõi",
+	"language" => "Ngôn Ngữ",
+	"subscription" => "Đăng Ký",
+	"firstname" => "Họ",
+	"lastname" => "Tên ",
+	"lastlogin"	=> "Đăng nhập lần cuối",
+	"personalinfo"	=> "Thông tin cá nhân",
+	"changepassword"	=> "Đổi Mật Khẩu",
+	"registernew" => "Đăng Ký Tài Khoản Mới ",
+	"backtosite" => " Quay lại trang chính ",
+	"planexpiration" => "Hạn",
+	'invitationmail' => 'Thư mời',
+
+/* grid , pagination */
+	"grid_displaying" 	=> "Đang hiển thị",
+	"grid_to" 			=> "Tới",
+	"grid_of" 			=> "Của",
+	"grid_show" 			=> "Hiển thị",
+	"grid_sort" 			=> "Sắp xếp",
+	"grid_order" 			=> "Phân loại",
+	"grid_page" 			=> "Trang",
+
+
+/* Menu navigation here */
+	"m_controlpanel"	=> "Trang Quản Lý",
+	"m_dashboard" 		=> "Dashboard",
+	"m_setting" 		=> "Cài Đặt", // updated apidevlab
+	"m_usersgroups" 	=> "Người dùng và nhóm",
+	"m_users" 			=> "Người Dùng",
+	"m_groups" 			=> "Nhóm",
+	"m_pagecms" 		=> "Trang CMS",
+	"m_menu" 			=> "Quản lý Menu",
+	"m_logs" 			=> "Hoạt động",
+	"m_codebuilder" 	=> "Code Generator",
+	"m_blastemail" 		=> "Blast Email",
+	"m_myaccount" 		=> "Tài khoản",
+	"m_logout" 			=> "Đăng xuất",
+	"m_profile" 		=> "Hồ sơ",
+	"m_manual" 		=> "Hướng dẫn thủ công ",
+
+/* Setting page translation */
+
+	"t_generalsetting"		=> "Hướng dẫn chung", //updated apidevlab
+	"t_generalsettingsmall"	=> "Cấu hình hướng dẫn chung", // updated apidevlab
+	"t_blastemail"			=> "Blast Email", // updated apidevlab
+	"t_blastemailsmall"		=> "Gửi Bulk Email",
+	"t_emailtemplate"		=> "Email Templates",
+	"t_emailtemplatesmall"	=> "Quản lý Email Templates", // updated apidevlab
+	"t_loginsecurity"		=> "Đăng nhập và Bảo vệ", // updated apidevlab
+	"t_loginsecuritysmall"	=> "Quản lý đăng nhập và bảo vệ",	// updated apidevlab
+	"t_socialmedia"			=> "Đăng nhập bằng mạng xã hội", // updated apidevlab
+	"t_lfb"					=> "Đăng nhập qua Facebook", // updated apidevlab
+	"t_lgoogle"				=> "Đăng nhập qua Google", // updated apidevlab
+	"t_ltwit"				=> "Đăng nhập qua Twitter", // updated apidevlab
+	"tab_siteinfo"			=> "Hướng dẫn chung", // Site Info updated apidevlab
+	"tab_loginsecurity"		=> "Đăng nhập và Bảo vệ",
+	"tab_email"				=> "Email Templates", // updated apidevlab
+	"tab_translation"			=> "Dịch",
+	"fr_appname"			=> "Tên ứng dụng ",
+	"fr_appdesc"			=> "Mô tả ứng dụng",
+	"fr_comname"			=> "Tên công ty ",
+	"fr_emailsys"			=> "Hệ thống email ",
+	"fr_emailmessage"		=> "Nội dung email ",
+	"fr_enable"				=> "Bật",
+	"fr_multilanguage"			=> "Đa ngôn ngữ",
+	"fr_mainlanguage"			=> "Ngôn ngữ chính",
+	"fr_fronttemplate"			=> "Giao diện trang chủ",
+	"fr_appmode"			=> "Application Mode",
+	"fr_appid"				=> "APP ID",
+	"fr_secret"				=> "SECRET NUMBER",
+	"fr_registrationdefault"		=> "Đăng ký nhóm mặc định ",
+	"fr_registrationsetting"	=> "Cài đặt đăng ký",
+	"fr_registration"		=> "Đăng ký",
+	"fr_allowregistration"		=> "Chấp nhận đăng ký",
+	"fr_allowfrontend"			=> "Chấp nhận trang chủ",
+	"fr_registrationauto"		=> "Tự động kích hoạt ",
+	"fr_registrationmanual"		=> "Kích hoạt thủ công ",
+	"fr_registrationemail"		=> "Email kích hoạt thủ công ",
+	"fr_emailsubject"				=> "Nhãn",
+	"fr_emailsendto"				=> "Gửi tới",
+	"fr_emailmessage"				=> "Nội dung email",
+	"fr_emailtag"					=> "Bạn có thể sử dụng",
+
+
+/* submit */
+	"sb_savechanges"			=> "Lưu thay đổi",
+	"sb_send"					=> "Gửi",
+	"sb_save"					=> "Lưu",
+	"sb_apply"					=> "Chấp nhận thay đổi",
+	"sb_submit"					=> "Trình",
+	"sb_cancel"					=> "Hủy",
+
+/* button */
+	"btn_back"						=> "Quay lại",
+	"btn_action"					=> "Hành động",
+	"btn_search"					=> "Tìm kiếm",
+	"btn_clearsearch"				=> "Xóa tìm kiếm",
+	"btn_download"					=> "Download",
+	"btn_config"					=> "Cấu hình",
+	"btn_copy"						=> "Copy", // Ajax
+	"btn_print"						=> "In",
+	"btn_create"					=> "Tạo",
+	"btn_install"					=> "Cài đặt",
+	"btn_backup"					=> "Backup",
+	"btn_remove"					=> "Xóa",
+	"btn_edit"						=> "Sửa",
+	"btn_view"						=> "View",
+	"btn_typesearch"				=> "Soạn và Enter",	// updated apidevlab
+
+/* Core Module */
+	"t_menu"						=> "Quản lý menu",
+	"t_menusmall"					=> "Danh sách menu	",
+	"t_tipsdrag"					=> "Kéo thả để sắp xếp menu", // updated apidevlab
+	"t_tipsnote"					=> "Chú ý! menu có 3 levels ", // updated apidevlab
+	"tab_topmenu"					=> "Menu top",
+	"tab_sidemenu"					=> "Menu Side ",
+	"sb_reorder"					=> "Sắp xếp lại menu",
+	"fr_mtitle"						=> "Tên/ Tiêu đề ",
+	"fr_mtype"						=> "Loại Menu",
+	"fr_mposition"					=> "Vị trí",
+	"fr_mactive"					=> "Đã kích hoạt",
+	"fr_minactive"					=> "Chưa kích hoạt",
+	"fr_maccess"					=> "Access", // updated apidevlab
+	"fr_miconclass"					=> "Lớp Icon",
+	"fr_mpublic"					=> "Xuất bản",
+	"fr_mexample"					=> "Ví dụ",
+	"fr_musage"						=> "Useage", // updated apidevlab
+
+/* Code BuilderModule */
+	"t_module"						=> "Module",
+	"t_modulesmall"					=> "Danh sách Modules",// updated apidevlab
+	"tab_installed"					=> "Những Modules đã cài đặt", // updated apidevlab
+	"tab_core"						=> "Core Modules", // updated apidevlab
+	"fr_modtitle"					=> "Tên/ Tiêu đề Module ",
+	"fr_modnote"					=> "Ghi chú Module ",
+	"fr_modtable"					=> "Bảng Module ",
+	"fr_modautosql"					=> "Tự động Mysql Statment",
+	"fr_modmanualsql"				=> "Thủ công Mysql Statment ",
+	"fr_createmodule"				=> "Tạo mới Module",
+	"fr_installmodule"				=> "Cài đặt Module",
+	"fr_backupmodule"				=> "Backup và tạo installer cho modules ",
+
+/* dashboard Interface */
+
+	"dash_i_module"					=> "Module",
+	"dash_i_setting"				=> "Cài đặt",
+	"dash_i_sitemenu"				=> "Trang Menu",
+	"dash_i_usergroup"				=> "Người dùng và nhóm",
+	"dash_module"					=> "Quản lý và tạo mới Module",
+	"dash_setting"					=> "Quản lý cài đặt ",
+	"dash_sitemenu"					=> "Quản lý các addon của frontend và backend",
+	"dash_usergroup"				=> "Quản lý các nhóm và người dùng",
+
+/*updates	on may ,5 2014 */
+
+	"loginsocial"				=> "Đăng nhập qua mạng xã hội", // updated apidevlab
+	"enteremailforgot"			=> "Điền thông tin email",
+	"detail" 					=> "Xem thông tin chi tiết",
+	"addedit" 					=> "Thêm - Sửa",
+
+/* Notification */
+	"note_noexists"				=> "Xin lỗi, Trang không tồn tại", // updated apidevlab
+	"note_restric"				=> "Xin lỗi, Bạn không có quyền truy cập trang này", // updated apidevlab
+	"note_success"				=> "Lưu thành công!", // updated apidevlab
+	"note_error"					=> "Xuất hiện lỗi !",
+	"note_success_delete"		=> "Xóa bỏ thành công!",	// updated apidevlab
+	"connect_stripe_note"		=> "Bạn cần kết nối với các loại trước",
+	"accept_payment"			=>"Thành viên không được chấp nhận thanh toán",
+);
